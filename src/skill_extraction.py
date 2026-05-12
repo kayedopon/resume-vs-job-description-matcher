9a5857112669b9022223ed94e24fe652f6f475b5
Kirill Postrehan <themrshayzoom882@gmail.com>
2026-05-12 22:05:32 +0300

    Add skill extraction functionality

---
+++ src/skill_extraction.py
@@ -0,0 +1,15 @@
+from src.preprocessing import clean_text
+
+import re
+
+
+def extract_skills(text, skill_list):
+  text = clean_text(text)
+  found_skills = []
+
+  for skill in skill_list:
+    pattern = r"\b" + re.escape(skill.lower()) + r"\b"
+    if re.search(pattern, text):
+      found_skills.append(skill)
+
+  return sorted(set(found_skills))
